b80861076ca09ec1414cbda42fb85c4399dfcfbc
Andrew Elkins <accounts@andrewelkins.com>
2012-05-22 11:30:52 -0700

    Massive delete

---
+++ WikiParser/plugins/PMWiki/pmwiki_outdented.class.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../interface/WikiParser_Library_StartOfLine.interface.php');
+require_once(dirname(__FILE__) . '/../interface/endOfLine.interface.php');
+
+class pmwiki_outdented implements WikiParser_Library_StartOfLineInterface, endOfLine
+{
+    const regular_expression = '/^-\</i';
+    private $enabled = false;
+    
+    public function __construct()
+    {
+        
+    }
+    
+    public function startOfLine($line)
+    {
+        return preg_replace_callback(pmwiki_outdented::regular_expression,array($this,'replace_callback'),$line);
+    }
+    
+    private function replace_callback($matches)
+    {
+        $this->enabled = true;
+        return "<div class=\"outdent\">" . substr($matches[0],2);
+    }
+    
+    public function endOfLine($line_of_text) 
+    {
+        if($this->enabled)
+        {
+            $this->enabled = false;
+            return $line_of_text . "</div>";
+        }
+        else
+        {
+            return $line_of_text;
+        }
+    }
+        
+}
+
+?>
